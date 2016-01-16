a408e8f7b707907bb1411deadfbfe182b0cbd836
Qi Xiao <xiaqqaix@gmail.com>
2016-01-15 19:08:48 -0500

    Expose Split

--- commandbot.go
+++ commandbot.go
@@ -10,7 +10,7 @@ type CommandBot struct {
 	AnyHandlers []AnyCommandHandler
 }
 
-func split(text string, sep byte) (string, string) {
+func Split(text string, sep byte) (string, string) {
 	var i, j int
 	for i = 0; i < len(text) && text[i] != sep; i++ {
 	}
@@ -31,8 +31,8 @@ func (b *CommandBot) HandleUpdate(_ *Bot, up *Update) {
 		// Not a command
 		return
 	}
-	cmd, args := split(text[1:len(text)], ' ')
-	cmd, to := split(cmd, '@')
+	cmd, args := Split(text[1:len(text)], ' ')
+	cmd, to := Split(cmd, '@')
 	if to != "" && to != b.Name {
 		// Not addressed to me
 		return
